62bb8b1946bdb1af0ced1b45296e23f999a7fdb6
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2024-01-24 18:48:13 +0100

    Start destination endpoint implementation

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -9,12 +9,15 @@ import (
 type MetricsValues struct {
 	Enabled               bool   `json:"enabled"`
 	AddonInstallNamespace string `json:"addonInstallNamespace"`
+	DestinationEndpoint   string `json:"destinationEndpoint"`
 }
 
 func GetValuesFunc(_ client.Client, _ *clusterv1.ManagedCluster, mca *addonapiv1alpha1.ManagedClusterAddOn) (MetricsValues, error) {
 	values := MetricsValues{
 		Enabled:               true,
 		AddonInstallNamespace: mca.Spec.InstallNamespace,
+		// TODO: grab Location from the `open-cluster-management-observability/observatorium-api` Route in the Hub
+		DestinationEndpoint: "",
 	}
 	return values, nil
 }
