83db164b4d7ed287d2c96c0fd9ae981af157ecae
Joao Marcal <jmarcal@redhat.com>
2024-05-06 10:34:20 +0100

    feat: addd controller to watch configuration resources

---
+++ cmd/watcher/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ViaQ/logerr/v2/log"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+
+	clfctrl "github.com/rhobs/multicluster-observability-addon/internal/controllers/watcher"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
+
+var scheme = runtime.NewScheme()
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	// +kubebuilder:scaffold:scheme
+}
+
+func main() {
+	var configFile string
+	flag.StringVar(&configFile, "config", "",
+		"The controller will load its initial configuration from this file. "+
+			"Omit this flag to use the default configuration values. "+
+			"Command-line flags override configuration from this file.",
+	)
+	flag.Parse()
+
+	logger := log.NewLogger("mcoa-watcher")
+	ctrl.SetLogger(logger)
+
+	var err error
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{Scheme: scheme})
+	if err != nil {
+		logger.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err = (&clfctrl.ClusterLogForwarderReconciler{
+		Client: mgr.GetClient(),
+		Log:    logger.WithName("controllers").WithName("mcoa-clf-watcher"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		logger.Error(err, "unable to create controller", "controller", "mcoa-clf-watcher")
+		os.Exit(1)
+	}
+
+	if err = mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
+		logger.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err = mgr.AddReadyzCheck("check", healthz.Ping); err != nil {
+		logger.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	logger.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		logger.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
