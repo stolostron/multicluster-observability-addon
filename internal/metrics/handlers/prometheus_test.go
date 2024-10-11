9dce1ca8362cb71723124011d3029a0f03cac121
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2024-10-11 18:21:29 +0200

    add prom builder tests
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

---
+++ internal/metrics/handlers/prometheus_test.go
@@ -0,0 +1,164 @@
+package handlers_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	prometheusv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	prometheusalpha1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
+	handler "github.com/rhobs/multicluster-observability-addon/internal/metrics/handlers"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestPrometheusAgentBuilder_EnforcedFields(t *testing.T) {
+	labelSelector := &metav1.LabelSelector{
+		MatchLabels: map[string]string{
+			"app": "test-app",
+		},
+	}
+	promAgent := &prometheusalpha1.PrometheusAgent{
+		Spec: prometheusalpha1.PrometheusAgentSpec{
+			CommonPrometheusFields: prometheusv1.CommonPrometheusFields{
+				// Enforced fields
+				ExternalLabels:                  map[string]string{"cluster": "other-cluster", "custom": "label"},
+				ScrapeConfigSelector:            labelSelector,
+				ServiceMonitorSelector:          labelSelector,
+				ServiceMonitorNamespaceSelector: labelSelector,
+				PodMonitorSelector:              labelSelector,
+				PodMonitorNamespaceSelector:     labelSelector,
+				ProbeSelector:                   labelSelector,
+				ProbeNamespaceSelector:          labelSelector,
+				Version:                         "invalid",
+				Image:                           toPtr("invalid"),
+				Replicas:                        toPtr(int32(10)),
+				Shards:                          toPtr(int32(10)),
+				EnableFeatures:                  []prometheusv1.EnableFeature{"tt"},
+				ExternalURL:                     "http://example.com",
+				ServiceAccountName:              "invalid",
+				AutomountServiceAccountToken:    toPtr(false),
+				ConfigMaps:                      []string{"invalid"},
+			},
+		},
+	}
+
+	builder := handler.PrometheusAgentBuilder{
+		Agent:                promAgent,
+		Name:                 "test-agent",
+		RemoteWriteEndpoint:  "https://example.com/write",
+		ClusterName:          "test-cluster",
+		ClusterID:            "test-cluster-id",
+		HAProxyConfigMapName: "haproxy-config",
+		HAProxyImage:         "haproxy:latest",
+		PrometheusImage:      "prometheus:latest",
+		MatchLabels:          map[string]string{"app": "test-app"},
+	}
+
+	result := builder.Build()
+
+	assert.Equal(t, int32(1), *result.Spec.CommonPrometheusFields.Replicas)
+	assert.True(t, builder.Agent.Spec.CommonPrometheusFields.ArbitraryFSAccessThroughSMs.Deny)
+
+	// External labels
+	assert.True(t, result.Spec.CommonPrometheusFields.OverrideHonorLabels)
+	assert.Len(t, result.Spec.CommonPrometheusFields.ExternalLabels, 3)
+	assert.Equal(t, "test-cluster", result.Spec.CommonPrometheusFields.ExternalLabels["cluster"])
+	assert.Equal(t, "test-cluster-id", result.Spec.CommonPrometheusFields.ExternalLabels["clusterID"])
+	assert.Equal(t, "label", result.Spec.CommonPrometheusFields.ExternalLabels["custom"])
+
+	// Remote write
+	assert.Len(t, result.Spec.CommonPrometheusFields.Secrets, 2)
+	assert.Equal(t, "https://example.com/write", result.Spec.CommonPrometheusFields.RemoteWrite[0].URL)
+	assert.Contains(t, builder.Agent.Spec.CommonPrometheusFields.RemoteWrite[0].TLSConfig.CAFile, "ca.crt")
+	assert.Contains(t, builder.Agent.Spec.CommonPrometheusFields.RemoteWrite[0].TLSConfig.CertFile, "tls.crt")
+	assert.Contains(t, builder.Agent.Spec.CommonPrometheusFields.RemoteWrite[0].TLSConfig.KeyFile, "tls.key")
+
+	// Version and Image
+	assert.Equal(t, "", result.Spec.CommonPrometheusFields.Version)
+	assert.Equal(t, "prometheus:latest", *result.Spec.CommonPrometheusFields.Image)
+
+	// Resources selectors, only ScrapeConfigNamespaceSelector is not enforced by the builder
+	assert.Equal(t, builder.MatchLabels, builder.Agent.Spec.CommonPrometheusFields.ScrapeConfigSelector.MatchLabels)
+	assert.Nil(t, builder.Agent.Spec.CommonPrometheusFields.ServiceMonitorNamespaceSelector)
+	assert.Nil(t, builder.Agent.Spec.CommonPrometheusFields.ServiceMonitorSelector)
+	assert.Nil(t, builder.Agent.Spec.CommonPrometheusFields.PodMonitorNamespaceSelector)
+	assert.Nil(t, builder.Agent.Spec.CommonPrometheusFields.PodMonitorSelector)
+	assert.Nil(t, builder.Agent.Spec.CommonPrometheusFields.ProbeNamespaceSelector)
+	assert.Nil(t, builder.Agent.Spec.CommonPrometheusFields.ProbeSelector)
+
+	// HAProxy sidecar
+	containers := builder.Agent.Spec.CommonPrometheusFields.Containers
+	assert.Len(t, containers, 1)
+	assert.Equal(t, "haproxy", containers[0].Name)
+	assert.Equal(t, "haproxy:latest", containers[0].Image)
+	assert.Len(t, builder.Agent.Spec.CommonPrometheusFields.Volumes, 2)
+	assert.Len(t, builder.Agent.Spec.CommonPrometheusFields.VolumeMounts, 0)
+
+}
+
+func TestPrometheusAgentBuilder_ConfigurableFields(t *testing.T) {
+	promAgent := &prometheusalpha1.PrometheusAgent{
+		Spec: prometheusalpha1.PrometheusAgentSpec{
+			CommonPrometheusFields: prometheusv1.CommonPrometheusFields{
+				ScrapeConfigNamespaceSelector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"app": "test-app",
+					},
+				},
+				Paused:                    true,
+				ImagePullPolicy:           corev1.PullNever,
+				LogLevel:                  "debug",
+				LogFormat:                 "json",
+				ScrapeInterval:            "1h",
+				ScrapeTimeout:             "1h",
+				EnableRemoteWriteReceiver: true,
+				Resources: corev1.ResourceRequirements{
+					Requests: corev1.ResourceList{
+						corev1.ResourceCPU: resource.MustParse("123m"),
+					},
+				},
+				NodeSelector: map[string]string{"node": "selector"},
+			},
+		},
+	}
+	builder := handler.PrometheusAgentBuilder{
+		Agent:                promAgent.DeepCopy(),
+		Name:                 "test-agent",
+		RemoteWriteEndpoint:  "https://example.com/write",
+		ClusterName:          "test-cluster",
+		ClusterID:            "test-cluster-id",
+		HAProxyConfigMapName: "haproxy-config",
+		HAProxyImage:         "haproxy:latest",
+		PrometheusImage:      "prometheus:latest",
+		MatchLabels:          map[string]string{"app": "test-app"},
+	}
+
+	builtSpec := reflect.ValueOf(builder.Build().Spec.CommonPrometheusFields)
+	configuredSpec := reflect.ValueOf(promAgent.Spec.CommonPrometheusFields)
+
+	for i := 0; i < configuredSpec.NumField(); i++ {
+		field := configuredSpec.Field(i)
+		fieldType := configuredSpec.Type().Field(i)
+		fmt.Printf("Field %s: %v\n", fieldType.Name, field.Interface())
+
+		if isZeroValue(field) {
+			continue
+		}
+
+		// Field should be the configured value
+		assert.Equal(t, field.Interface(), builtSpec.Field(i).Interface())
+	}
+
+}
+
+func isZeroValue(v reflect.Value) bool {
+	zeroValue := reflect.Zero(v.Type()).Interface()
+	return reflect.DeepEqual(v.Interface(), zeroValue)
+}
+
+func toPtr[T any](v T) *T {
+	return &v
+}
