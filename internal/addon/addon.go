f4a0491c4488a58845653c90bcecfaab0afb5f60
Joao Marcal <jmarcal@redhat.com>
2025-04-03 17:13:24 +0100

    Add probe for platform PrometheusAgent

--- internal/addon/addon.go
+++ internal/addon/addon.go
@@ -6,6 +6,7 @@ import (
 
 	otelv1alpha1 "github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
 	loggingv1 "github.com/openshift/cluster-logging-operator/api/observability/v1"
+	prometheusalpha1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1alpha1"
 	"open-cluster-management.io/addon-framework/pkg/agent"
 	"open-cluster-management.io/addon-framework/pkg/utils"
 	"open-cluster-management.io/api/addon/v1alpha1"
@@ -35,6 +36,25 @@ func AgentHealthProber() *agent.HealthProber {
 		Type: agent.HealthProberTypeWork,
 		WorkProber: &agent.WorkHealthProber{
 			ProbeFields: []agent.ProbeField{
+				{
+					ResourceIdentifier: workv1.ResourceIdentifier{
+						Group:     prometheusalpha1.SchemeGroupVersion.Group,
+						Resource:  PrometheusAgentResource,
+						Name:      PPAName,
+						Namespace: InstallNamespace,
+					},
+					ProbeRules: []workv1.FeedbackRule{
+						{
+							Type: workv1.JSONPathsType,
+							JsonPaths: []workv1.JsonPath{
+								{
+									Name: paProbeKey,
+									Path: paProbePath,
+								},
+							},
+						},
+					},
+				},
 				{
 					ResourceIdentifier: workv1.ResourceIdentifier{
 						Group:     loggingv1.GroupVersion.Group,
@@ -80,11 +100,26 @@ func AgentHealthProber() *agent.HealthProber {
 				}
 				for _, field := range fields {
 					if len(field.FeedbackResult.Values) == 0 {
-						// If a probe didn't get any values maybe the resources was not deployed
+						// If a probe didn't get any values maybe the resources were not deployed
 						continue
 					}
 					identifier := field.ResourceIdentifier
 					switch identifier.Resource {
+					case PrometheusAgentResource:
+						for _, value := range field.FeedbackResult.Values {
+							if value.Name != paProbeKey {
+								return fmt.Errorf("%w: %s with key %s/%s unknown probe keys %s", errUnknownProbeKey, identifier.Resource, identifier.Namespace, identifier.Name, value.Name)
+							}
+
+							if value.Value.String == nil {
+								return fmt.Errorf("%w: %s with key %s/%s", errProbeValueIsNil, identifier.Resource, identifier.Namespace, identifier.Name)
+							}
+
+							if *value.Value.String != "True" {
+								return fmt.Errorf("%w: %s status condition type is %s for %s/%s", errProbeConditionNotSatisfied, identifier.Resource, *value.Value.String, identifier.Namespace, identifier.Name)
+							}
+							// pa passes the health check
+						}
 					case ClusterLogForwardersResource:
 						for _, value := range field.FeedbackResult.Values {
 							if value.Name != clfProbeKey {
@@ -98,7 +133,7 @@ func AgentHealthProber() *agent.HealthProber {
 							if *value.Value.String != "True" {
 								return fmt.Errorf("%w: %s status condition type is %s for %s/%s", errProbeConditionNotSatisfied, identifier.Resource, *value.Value.String, identifier.Namespace, identifier.Name)
 							}
-							// everything checks we should skip to the next field
+							// clf passes the health check
 						}
 					case OpenTelemetryCollectorsResource:
 						for _, value := range field.FeedbackResult.Values {
@@ -113,7 +148,7 @@ func AgentHealthProber() *agent.HealthProber {
 							if *value.Value.Integer < 1 {
 								return fmt.Errorf("%w: %s replicas is %d for %s/%s", errProbeConditionNotSatisfied, identifier.Resource, *value.Value.Integer, identifier.Namespace, identifier.Name)
 							}
-							// everything checks we should skip to the next field
+							// otel collector passes the health check
 						}
 					default:
 						// If a resource is being probed it should have a health check defined
--- internal/addon/addon_test.go
+++ internal/addon/addon_test.go
@@ -12,6 +12,58 @@ import (
 	workv1 "open-cluster-management.io/api/work/v1"
 )
 
+func Test_AgentHealthProber_PPA(t *testing.T) {
+	unhealthyError := fmt.Errorf("%w: prometheusagents status condition type is %s for %s/%s", errProbeConditionNotSatisfied, "False", InstallNamespace, PPAName)
+	managedCluster := addontesting.NewManagedCluster("cluster-1")
+	managedClusterAddOn := addontesting.NewAddon("test", "cluster-1")
+	for _, tc := range []struct {
+		name        string
+		status      string
+		expectedErr string
+	}{
+		{
+			name:   "healthy",
+			status: "True",
+		},
+		{
+			name:        "unhealthy",
+			status:      "False",
+			expectedErr: unhealthyError.Error(),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			healthProber := AgentHealthProber()
+			err := healthProber.WorkProber.HealthChecker(
+				[]agent.FieldResult{
+					{
+						ResourceIdentifier: workv1.ResourceIdentifier{
+							Group:     loggingv1.GroupVersion.Group,
+							Resource:  PrometheusAgentResource,
+							Name:      PPAName,
+							Namespace: InstallNamespace,
+						},
+						FeedbackResult: workv1.StatusFeedbackResult{
+							Values: []workv1.FeedbackValue{
+								{
+									Name: "isAvailable",
+									Value: workv1.FieldValue{
+										Type:   workv1.String,
+										String: &tc.status,
+									},
+								},
+							},
+						},
+					},
+				}, managedCluster, managedClusterAddOn)
+			if tc.expectedErr != "" {
+				require.EqualError(t, err, tc.expectedErr)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
+
 func Test_AgentHealthProber_CLF(t *testing.T) {
 	unhealthyError := fmt.Errorf("%w: clusterlogforwarders status condition type is %s for %s/%s", errProbeConditionNotSatisfied, "False", SpokeCLFNamespace, SpokeCLFName)
 	managedCluster := addontesting.NewManagedCluster("cluster-1")
