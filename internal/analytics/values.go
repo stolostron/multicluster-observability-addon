abb32b324a5e0e610ccc57b853edaa9fa0bfc1f4
Joao Marcal <jmarcal@redhat.com>
2025-04-24 14:25:08 +0100

    refactor: values now are pointers making the GetValues function simpler (#148)

--- internal/analytics/values.go
+++ internal/analytics/values.go
@@ -3,5 +3,5 @@ package analytics
 import "github.com/stolostron/multicluster-observability-addon/internal/analytics/incident-detection/manifests"
 
 type AnalyticsValues struct {
-	IncidentDetectionValues manifests.IncidentDetectionValues `json:"incidentDetection"`
+	IncidentDetectionValues *manifests.IncidentDetectionValues `json:"incidentDetection,omitempty"`
 }
