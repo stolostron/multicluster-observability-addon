6d290f98d2e7b839b119a251e0ae3da69c082889
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2025-04-30 08:03:18 +0200

    update
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- internal/addon/options.go
+++ internal/addon/options.go
@@ -185,6 +185,8 @@ func BuildOptions(addOnDeployment *addonapiv1alpha1.AddOnDeploymentConfig) (Opti
 				opts.ObsUI.Enabled = true
 			}
 		case KeyObservabilityUILogs:
+			if keyvalue.Value == string(UIPluginV1alpha1) && opts.ObsUI.Enabled {
+				opts.ObsUI.Logs.Enabled = true
 			}
 		}
 	}
