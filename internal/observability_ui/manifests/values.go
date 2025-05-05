bd36e3241aa3c5841251f8d60acd227001ad7301
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2025-05-05 16:24:08 +0200

    refactor
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- internal/observability_ui/manifests/values.go
+++ internal/observability_ui/manifests/values.go
@@ -1,9 +1,38 @@
 package manifests
 
 type UIValues struct {
+	Enabled bool            `json:"enabled"`
+	Logs    LogsUIValues    `json:"logging"`
+	Metrics MetricsUIValues `json:"metrics"`
+}
+
+type LogsUIValues struct {
+	Enabled bool `json:"enabled"`
+}
+
+type MetricsUIValues struct {
+	Enabled bool         `json:"enabled"`
+	ACM     ACMValues    `json:"acm"`
+	Perses  PersesValues `json:"perses"`
+}
+
+type ACMValues struct {
+	Enabled bool `json:"enabled"`
+}
+
+type PersesValues struct {
 	Enabled bool `json:"enabled"`
 }
 
 func BuildValues(opts Options) *UIValues {
-	return &UIValues{opts.Enabled}
+	values := &UIValues{
+		Enabled: opts.Enabled,
+		Logs:    LogsUIValues{Enabled: opts.LogsUI.Enabled},
+		Metrics: MetricsUIValues{
+			Enabled: opts.MetricsUI.Enabled,
+			ACM:     ACMValues{Enabled: opts.MetricsUI.ACM.Enabled},
+			Perses:  PersesValues{Enabled: opts.MetricsUI.Perses.Enabled},
+		},
+	}
+	return values
 }
