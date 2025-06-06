a1ea91c2271cab8bf02e0ba2d1c5319dd05ec69e
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2025-06-06 11:33:14 +0200

    use default queue config
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- internal/metrics/resource/agentssa.go
+++ internal/metrics/resource/agentssa.go
@@ -157,16 +157,6 @@ func (p *PrometheusAgentSSA) setPrometheusRemoteWriteConfig() {
 			KeyFile:  p.formatSecretPath(config.ClientCertSecretName, "tls.key"),
 		},
 		// WriteRelabelConfigs is set individually for each managed cluster in order to enforce cluster identification labels
-		QueueConfig: &prometheusv1.QueueConfig{
-			BatchSendDeadline: ptr.To(prometheusv1.Duration("15s")),
-			Capacity:          12000,
-			MaxShards:         3,
-			MinShards:         1,
-			MaxSamplesPerSend: 4000,
-			MinBackoff:        ptr.To(prometheusv1.Duration("1s")),
-			MaxBackoff:        ptr.To(prometheusv1.Duration("30s")),
-			RetryOnRateLimit:  true,
-		},
 	}
 
 	// Ensure there is a single instance of our config
