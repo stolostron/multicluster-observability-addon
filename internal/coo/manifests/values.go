ff4b268b7d97144943847b40e79b8d058e5cc0c7
Joao Marcal <jmarcal@redhat.com>
2025-07-28 10:20:30 +0100

    chore: refactor coo values.go to make it simpler (#285)

--- internal/coo/manifests/values.go
+++ internal/coo/manifests/values.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/perses/perses/go-sdk/dashboard"
 	"github.com/stolostron/multicluster-observability-addon/internal/addon"
+	"github.com/stolostron/multicluster-observability-addon/internal/addon/config"
 	imanifests "github.com/stolostron/multicluster-observability-addon/internal/analytics/incident-detection/manifests"
 	mmanifests "github.com/stolostron/multicluster-observability-addon/internal/metrics/manifests"
 	"github.com/stolostron/multicluster-observability-addon/internal/perses/dashboards/acm"
@@ -13,8 +14,7 @@ import (
 )
 
 var (
-	project          = "open-cluster-management-observability"
-	datasource       = "thanos-query-frontend"
+	dsThanos         = "thanos-query-frontend"
 	clusterLabelName = ""
 )
 
@@ -40,9 +40,59 @@ type COOValues struct {
 	IncidentDetection  *imanifests.IncidentDetectionValues `json:"incidentDetection,omitempty"`
 }
 
-func buildACMDashboards() []DashboardValue {
+func BuildValues(opts addon.Options, installCOO bool, isHubCluster bool) *COOValues {
 	var dashboards []DashboardValue
 
+	metricsUI := mmanifests.EnableUI(opts.Platform.Metrics, isHubCluster)
+	if metricsUI != nil {
+		if metricsUI.Enabled {
+			dashboards = append(dashboards, buildACMDashboards()...)
+		}
+	}
+
+	incidentDetection := imanifests.EnableUI(opts.Platform.AnalyticsOptions.IncidentDetection)
+	if incidentDetection != nil {
+		if incidentDetection.Enabled {
+			dashboards = append(dashboards, buildIncidentDetetctionDashboards()...)
+		}
+	}
+
+	return &COOValues{
+		// Decide if this chart is needed
+		Enabled: len(dashboards) > 0,
+		// Decide if COO chart is needs to be installed
+		InstallCOO:         installCOO,
+		MonitoringUIPlugin: len(dashboards) > 0,
+		Dashboards:         dashboards,
+		Metrics:            metricsUI,
+		IncidentDetection:  incidentDetection,
+	}
+}
+
+func buildDashboards(builders []DashboardBuilder, datasource string) []DashboardValue {
+	var dashboards []DashboardValue
+
+	for _, builder := range builders {
+		db, err := builder.fn(config.InstallNamespace, datasource, clusterLabelName)
+		if err != nil {
+			log.Printf("Failed to build %s dashboard: %v", builder.name, err)
+			continue
+		}
+		data, err := json.Marshal(db.Dashboard.Spec)
+		if err != nil {
+			log.Printf("Failed to marshal %s dashboard: %v", builder.name, err)
+			continue
+		}
+		dashboards = append(dashboards, DashboardValue{
+			Name: db.Dashboard.Metadata.Name,
+			Data: string(data),
+		})
+	}
+
+	return dashboards
+}
+
+func buildACMDashboards() []DashboardValue {
 	builders := []DashboardBuilder{
 		{acm.BuildClusterResourceUse, "ClusterResourceUse"},
 		{acm.BuildNodeResourceUse, "NodeResourceUse"},
@@ -50,82 +100,13 @@ func buildACMDashboards() []DashboardValue {
 		{acm.BuildACMClustersOverview, "ACMClustersOverview"},
 	}
 
-	for _, builder := range builders {
-		db, err := builder.fn(project, datasource, clusterLabelName)
-		if err != nil {
-			log.Printf("Failed to build %s dashboard: %v", builder.name, err)
-			continue
-		}
-		data, err := json.Marshal(db.Dashboard.Spec)
-		if err != nil {
-			log.Printf("Failed to marshal %s dashboard: %v", builder.name, err)
-			continue
-		}
-		dashboards = append(dashboards, DashboardValue{
-			Name: db.Dashboard.Metadata.Name,
-			Data: string(data),
-		})
-	}
-
-	return dashboards
+	return buildDashboards(builders, dsThanos)
 }
 
 func buildIncidentDetetctionDashboards() []DashboardValue {
-	var dashboards []DashboardValue
-
 	builders := []DashboardBuilder{
 		{incident_management.BuildACMIncidentsOverview, "IncidentDetectionOverview"},
 	}
 
-	for _, builder := range builders {
-		db, err := builder.fn(project, datasource, clusterLabelName)
-		if err != nil {
-			log.Printf("Failed to build %s dashboard: %v", builder.name, err)
-			continue
-		}
-		data, err := json.Marshal(db.Dashboard.Spec)
-		if err != nil {
-			log.Printf("Failed to marshal %s dashboard: %v", builder.name, err)
-			continue
-		}
-		dashboards = append(dashboards, DashboardValue{
-			Name: db.Dashboard.Metadata.Name,
-			Data: string(data),
-		})
-	}
-
-	return dashboards
-}
-
-func BuildValues(opts addon.Options, installCOO bool, isHubCluster bool) *COOValues {
-	metricsUI := mmanifests.EnableUI(opts.Platform.Metrics, isHubCluster)
-
-	incidentDetection := imanifests.EnableUI(opts.Platform.AnalyticsOptions.IncidentDetection)
-
-	var dashboards []DashboardValue
-
-	monitoringUIPlugin := false
-	if metricsUI != nil {
-		monitoringUIPlugin = metricsUI.Enabled
-		if metricsUI.Enabled {
-			dashboards = append(dashboards, buildACMDashboards()...)
-		}
-	}
-
-	if incidentDetection != nil {
-		monitoringUIPlugin = monitoringUIPlugin || incidentDetection.Enabled
-		if incidentDetection.Enabled {
-			dashboards = append(dashboards, buildIncidentDetetctionDashboards()...)
-		}
-	}
-
-	return &COOValues{
-		// Decide if COO chart is needed
-		Enabled:            monitoringUIPlugin,
-		InstallCOO:         installCOO,
-		MonitoringUIPlugin: monitoringUIPlugin,
-		Dashboards:         dashboards,
-		Metrics:            metricsUI,
-		IncidentDetection:  incidentDetection,
-	}
+	return buildDashboards(builders, dsThanos)
 }
