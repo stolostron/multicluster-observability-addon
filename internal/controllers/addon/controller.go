e75923aaaf9d31ab26fc0f57a6c90c354916e839
Coleen Iona Quadros <coleen.quadros27@gmail.com>
2025-10-22 15:00:26 +0200

    add install ns (#323)
    Signed-off-by: Coleen Iona Quadros <coleen.quadros27@gmail.com>

--- internal/controllers/addon/controller.go
+++ internal/controllers/addon/controller.go
@@ -83,7 +83,12 @@ func NewAddonManager(ctx context.Context, kubeConfig *rest.Config, scheme *runti
 		WithGetValuesFuncs(addonConfigValuesFn, addonhelm.GetValuesFunc(ctx, k8sClient, agentLogger)).
 		WithUpdaters(addon.Updaters()).
 		WithAgentRegistrationOption(registrationOption).
-		WithScheme(scheme).
+		WithAgentInstallNamespace(
+			// Set agent install namespace from addon deployment config if it exists
+			utils.AgentInstallNamespaceFromDeploymentConfigFunc(
+				utils.NewAddOnDeploymentConfigGetter(addonClient),
+			),
+		).WithScheme(scheme).
 		BuildHelmAgentAddon()
 	if err != nil {
 		return nil, fmt.Errorf("failed to build helm agent addon: %w", err)
