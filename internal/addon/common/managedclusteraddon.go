8287c579ba8a62a71bbac35205142512fa011cc7
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2026-01-23 16:28:20 +0100

    refactor(addon): centralize AODC retrieval logic
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- internal/addon/common/managedclusteraddon.go
+++ internal/addon/common/managedclusteraddon.go
@@ -1,8 +1,18 @@
 package common
 
 import (
+	"context"
+	"errors"
+
+	addoncfg "github.com/stolostron/multicluster-observability-addon/internal/addon/config"
+	addonutils "open-cluster-management.io/addon-framework/pkg/utils"
 	addonapiv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var (
+	ErrMissingAODCRef  = errors.New("missing required AddOnDeploymentConfig reference in addon configuration")
+	ErrMultipleAODCRef = errors.New("addonmultiple AddOnDeploymentConfig references found - only one is supported")
 )
 
 func GetObjectKeys(configRef []addonapiv1alpha1.ConfigReference, group, resource string) []client.ObjectKey {
@@ -15,10 +25,35 @@ func GetObjectKeys(configRef []addonapiv1alpha1.ConfigReference, group, resource
 			continue
 		}
 
-		keys = append(keys, client.ObjectKey{
-			Name:      config.Name,
-			Namespace: config.Namespace,
-		})
+		if config.DesiredConfig != nil {
+			keys = append(keys, client.ObjectKey{
+				Name:      config.DesiredConfig.Name,
+				Namespace: config.DesiredConfig.Namespace,
+			})
+		} else {
+			// These fields are deprecated
+			keys = append(keys, client.ObjectKey{
+				Name:      config.Name,
+				Namespace: config.Namespace,
+			})
+		}
+
 	}
 	return keys
+}
+
+func GetAddOnDeploymentConfig(ctx context.Context, k8s client.Client, mcAddon *addonapiv1alpha1.ManagedClusterAddOn) (*addonapiv1alpha1.AddOnDeploymentConfig, error) {
+	aodc := &addonapiv1alpha1.AddOnDeploymentConfig{}
+	keys := GetObjectKeys(mcAddon.Status.ConfigReferences, addonutils.AddOnDeploymentConfigGVR.Group, addoncfg.AddonDeploymentConfigResource)
+	switch {
+	case len(keys) == 0:
+		return aodc, ErrMissingAODCRef
+	case len(keys) > 1:
+		return aodc, ErrMultipleAODCRef
+	}
+	if err := k8s.Get(ctx, keys[0], aodc, &client.GetOptions{}); err != nil {
+		// TODO(JoaoBraveCoding) Add proper error handling
+		return aodc, err
+	}
+	return aodc, nil
 }
