f3c42924365831e27ba448d2818c6b6e53d2e70e
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2026-02-09 15:16:50 +0100

    fix lint
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- internal/perses/dashboards/acm/dash_k8s_test.go
+++ internal/perses/dashboards/acm/dash_k8s_test.go
@@ -10,7 +10,7 @@ func TestBuildK8sDashboards(t *testing.T) {
 	// Call it once
 	_, err := BuildK8sDashboards("test-project", "test-datasource", "test-cluster-label")
 	require.NoError(t, err)
-	
+
 	// Call it again - this should not panic
 	_, err = BuildK8sDashboards("test-project", "test-datasource", "test-cluster-label")
 	require.NoError(t, err)
