be905a36098dc95d6d17905c8caec2afea98a603
Darshan Vandra <dvandra@redhat.com>
2026-04-20 18:49:36 +0530

    add perses dashboards for right-sizing
    Signed-off-by: Darshan Vandra <dvandra@redhat.com>

---
+++ internal/perses/dashboards/rightsizing/vm-overestimation.go
@@ -0,0 +1,140 @@
+// Copyright (c) Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+// Licensed under the Apache License 2.0
+
+package rightsizing
+
+import (
+	"time"
+
+	"github.com/perses/community-mixins/pkg/dashboards"
+	"github.com/perses/community-mixins/pkg/promql"
+	"github.com/perses/perses/go-sdk/dashboard"
+	panelgroup "github.com/perses/perses/go-sdk/panel-group"
+	listVar "github.com/perses/perses/go-sdk/variable/list-variable"
+	labelValuesVar "github.com/perses/plugins/prometheus/sdk/go/variable/label-values"
+	staticListVar "github.com/perses/plugins/staticlistvariable/sdk/go"
+	acmHelpers "github.com/stolostron/multicluster-observability-addon/internal/perses/dashboards/acm"
+	panels "github.com/stolostron/multicluster-observability-addon/internal/perses/panels/rightsizing"
+)
+
+// BuildVMOverestimation creates the VM overestimation detail dashboard
+func BuildVMOverestimation(project string, datasource string, clusterLabelName string) (dashboard.Builder, error) {
+	return dashboard.New("acm-rightsizing-vm-overestimation",
+		dashboard.ProjectName(project),
+		dashboard.Name("ACM Right-Sizing OpenShift Virtualization VM Overestimation"),
+		dashboard.Duration(time.Hour*24*7),
+
+		dashboard.AddVariable("cluster",
+			listVar.List(
+				labelValuesVar.PrometheusLabelValues("cluster",
+					dashboards.AddVariableDatasource(datasource),
+					labelValuesVar.Matchers(
+						promql.SetLabelMatchers(
+							`{__name__=~"acm_rs_vm:namespace:(cpu_request|cpu_usage|memory_request|memory_usage)"}`,
+							[]promql.LabelMatcher{},
+						)),
+				),
+				listVar.DisplayName("Cluster"),
+				listVar.AllowAllValue(false),
+				listVar.AllowMultiple(false),
+			),
+		),
+
+		dashboard.AddVariable("profile",
+			listVar.List(
+				labelValuesVar.PrometheusLabelValues("profile",
+					dashboards.AddVariableDatasource(datasource),
+					labelValuesVar.Matchers(
+						promql.SetLabelMatchers(
+							`{__name__=~"acm_rs_vm:namespace:(cpu_request|cpu_usage|memory_request|memory_usage)",cluster="$cluster"}`,
+							[]promql.LabelMatcher{},
+						)),
+				),
+				listVar.DisplayName("Profile"),
+				listVar.DefaultValue("Max OverAll"),
+				listVar.AllowAllValue(false),
+				listVar.AllowMultiple(false),
+			),
+		),
+
+		dashboard.AddVariable("days",
+			listVar.List(
+				staticListVar.StaticList(
+					staticListVar.Values("1d", "2d", "5d", "10d", "15d", "30d", "60d", "90d"),
+				),
+				listVar.DisplayName("Aggregation"),
+				listVar.DefaultValue("5d"),
+				listVar.AllowAllValue(false),
+				listVar.AllowMultiple(false),
+			),
+		),
+
+		dashboard.AddVariable("namespace",
+			listVar.List(
+				labelValuesVar.PrometheusLabelValues("namespace",
+					dashboards.AddVariableDatasource(datasource),
+					labelValuesVar.Matchers(
+						promql.SetLabelMatchers(
+							`{__name__=~"acm_rs_vm:namespace:(cpu_request|cpu_usage|memory_request|memory_usage)",cluster="$cluster",profile="$profile"}`,
+							[]promql.LabelMatcher{},
+						)),
+				),
+				listVar.DisplayName("Namespace"),
+				listVar.AllowAllValue(false),
+				listVar.AllowMultiple(false),
+			),
+		),
+
+		dashboard.AddVariable("vm",
+			listVar.List(
+				labelValuesVar.PrometheusLabelValues("name",
+					dashboards.AddVariableDatasource(datasource),
+					labelValuesVar.Matchers(
+						promql.SetLabelMatchers(
+							`{__name__=~"acm_rs_vm:namespace:(cpu_request|cpu_usage|memory_request|memory_usage)",cluster="$cluster",profile="$profile",namespace="$namespace"}`,
+							[]promql.LabelMatcher{},
+						)),
+				),
+				listVar.DisplayName("VM"),
+				listVar.AllowAllValue(false),
+				listVar.AllowMultiple(false),
+			),
+		),
+
+		acmHelpers.AddCustomPanelGroup("",
+			[]acmHelpers.GridItem{{X: 17, Y: 0, W: 7, H: 2}},
+			panels.VMBackToMainDashboardPanel(datasource, project),
+		),
+
+		dashboard.AddPanelGroup("",
+			panelgroup.PanelsPerLine(4),
+			panelgroup.PanelHeight(3),
+			panels.VMCPUOverestimationStatPanel(datasource),
+			panels.VMCPUUsageStatPanel(datasource),
+			panels.VMCPURequestStatPanel(datasource),
+			panels.VMCPUUtilizationStatPanel(datasource),
+		),
+
+		dashboard.AddPanelGroup("",
+			panelgroup.PanelsPerLine(1),
+			panelgroup.PanelHeight(12),
+			panels.VMCPUUtilizationTimeSeriesPanel(datasource),
+		),
+
+		dashboard.AddPanelGroup("",
+			panelgroup.PanelsPerLine(4),
+			panelgroup.PanelHeight(3),
+			panels.VMMemoryOverestimationStatPanel(datasource),
+			panels.VMMemoryUsageStatPanel(datasource),
+			panels.VMMemoryRequestStatPanel(datasource),
+			panels.VMMemoryUtilizationStatPanel(datasource),
+		),
+
+		dashboard.AddPanelGroup("",
+			panelgroup.PanelsPerLine(1),
+			panelgroup.PanelHeight(12),
+			panels.VMMemoryUtilizationTimeSeriesPanel(datasource),
+		),
+	)
+}
---
+++ internal/perses/panels/rightsizing/namespace-panels.go
@@ -0,0 +1,328 @@
+// Copyright (c) Red Hat, Inc.
+// Copyright Contributors to the Open Cluster Management project
+// Licensed under the Apache License 2.0
+
+package rightsizing
+
+import (
+	"github.com/perses/community-mixins/pkg/dashboards"
+	commonSdk "github.com/perses/perses/go-sdk/common"
+	"github.com/perses/perses/go-sdk/panel"
+	panelgroup "github.com/perses/perses/go-sdk/panel-group"
+	"github.com/perses/plugins/prometheus/sdk/go/query"
+	tablePanel "github.com/perses/plugins/table/sdk/go"
+	timeSeriesPanel "github.com/perses/plugins/timeserieschart/sdk/go"
+)
+
+// nsTblCol is a shorthand to create a ColumnSettingsWithLink for namespace tables (no DataLink).
+func nsTblCol(name, header string, align tablePanel.Align, format *commonSdk.Format, opts ...func(*ColumnSettingsWithLink)) ColumnSettingsWithLink {
+	c := ColumnSettingsWithLink{
+		ColumnSettings: tablePanel.ColumnSettings{
+			Name:          name,
+			Header:        header,
+			Align:         align,
+			Format:        format,
+			EnableSorting: true,
+		},
+	}
+	for _, fn := range opts {
+		fn(&c)
+	}
+	return c
+}
+
+var greenThreshold = &commonSdk.Thresholds{
+	Steps: []commonSdk.StepOption{
+		{Value: 0, Color: "green"},
+	},
+}
+
+var grayThreshold = &commonSdk.Thresholds{
+	Steps: []commonSdk.StepOption{
+		{Value: 0, Color: "#808080"},
+	},
+}
+
+var percentThreshold = &commonSdk.Thresholds{
+	Steps: []commonSdk.StepOption{
+		{Value: 0, Color: "#808080"},
+	},
+}
+
+func CPURecommendationPanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "CPU Recommendation",
+		Description: "CPU recommendation for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:cpu_recommendation{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.DecimalUnit,
+		Decimals:    2,
+		FontSize:    48,
+		Thresholds:  greenThreshold,
+	})
+}
+
+func CPUUsagePanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "CPU Usage",
+		Description: "CPU usage for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:cpu_usage{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.DecimalUnit,
+		Decimals:    2,
+		FontSize:    48,
+		Thresholds:  grayThreshold,
+	})
+}
+
+func CPURequestPanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "CPU Request",
+		Description: "CPU request for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:cpu_request{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.DecimalUnit,
+		Decimals:    2,
+		FontSize:    48,
+		Thresholds:  grayThreshold,
+	})
+}
+
+func CPUUtilizationPanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "CPU Utilization",
+		Description: "CPU utilization percentage for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:cpu_usage{cluster="$cluster", profile="$profile"})[$days:]) / max_over_time(sum by (cluster)(acm_rs:cluster:cpu_request{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.PercentDecimalUnit,
+		Decimals:    1,
+		FontSize:    48,
+		Thresholds:  percentThreshold,
+	})
+}
+
+func MemRecommendationPanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "Mem Recommendation",
+		Description: "Memory recommendation for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:memory_recommendation{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.BytesUnit,
+		Decimals:    1,
+		FontSize:    40,
+		Thresholds:  greenThreshold,
+	})
+}
+
+func MemUsagePanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "Mem Usage",
+		Description: "Memory usage for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:memory_usage{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.BytesUnit,
+		Decimals:    1,
+		FontSize:    40,
+		Thresholds:  grayThreshold,
+	})
+}
+
+func MemRequestPanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "Mem Request",
+		Description: "Memory request for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:memory_request{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.BytesUnit,
+		Decimals:    1,
+		FontSize:    40,
+		Thresholds:  grayThreshold,
+	})
+}
+
+func MemUtilizationPanel(datasourceName string) panelgroup.Option {
+	return BuildStatPanel(datasourceName, StatPanelConfig{
+		Title:       "Mem Utilization",
+		Description: "Memory utilization percentage for the selected cluster",
+		Query:       `max_over_time(sum by (cluster)(acm_rs:cluster:memory_usage{cluster="$cluster", profile="$profile"})[$days:]) / max_over_time(sum by (cluster)(acm_rs:cluster:memory_request{cluster="$cluster", profile="$profile"})[$days:])`,
+		Unit:        &dashboards.PercentDecimalUnit,
+		Decimals:    1,
+		FontSize:    40,
+		Thresholds:  percentThreshold,
+	})
+}
+
+func CPUTopNamespacesPanel(datasourceName string) panelgroup.Option {
+	return panelgroup.AddPanel("CPU Utilization of Top Namespaces",
+		panel.Description("CPU utilization of the top 20 namespaces by usage/request ratio"),
+		timeSeriesPanel.Chart(
+			timeSeriesPanel.WithYAxis(timeSeriesPanel.YAxis{
+				Format: &commonSdk.Format{
+					Unit: &dashboards.PercentDecimalUnit,
+				},
+			}),
+			timeSeriesPanel.WithLegend(timeSeriesPanel.Legend{
+				Position: timeSeriesPanel.BottomPosition,
+				Mode:     timeSeriesPanel.ListMode,
+			}),
+			timeSeriesPanel.WithVisual(timeSeriesPanel.Visual{
+				Display:      timeSeriesPanel.LineDisplay,
+				ConnectNulls: true,
+				LineWidth:    1.25,
+				AreaOpacity:  0.4,
+				PointRadius:  2.75,
+			}),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`topk(20, sum by (namespace) (acm_rs:namespace:cpu_usage{cluster="$cluster", profile="$profile"}) / sum by (namespace) (acm_rs:namespace:cpu_request{cluster="$cluster", profile="$profile"}))`,
+				dashboards.AddQueryDataSource(datasourceName),
+				query.SeriesNameFormat("{{namespace}}"),
+			),
+		),
+	)
+}
+
+func MemTopNamespacesPanel(datasourceName string) panelgroup.Option {
+	return panelgroup.AddPanel("Memory Utilization of Top Namespaces",
+		panel.Description("Memory utilization of the top 20 namespaces by usage/request ratio"),
+		timeSeriesPanel.Chart(
+			timeSeriesPanel.WithYAxis(timeSeriesPanel.YAxis{
+				Format: &commonSdk.Format{
+					Unit: &dashboards.PercentDecimalUnit,
+				},
+			}),
+			timeSeriesPanel.WithLegend(timeSeriesPanel.Legend{
+				Position: timeSeriesPanel.BottomPosition,
+				Mode:     timeSeriesPanel.ListMode,
+			}),
+			timeSeriesPanel.WithVisual(timeSeriesPanel.Visual{
+				Display:      timeSeriesPanel.LineDisplay,
+				ConnectNulls: true,
+				LineWidth:    1.25,
+				AreaOpacity:  0.4,
+				PointRadius:  2.75,
+			}),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`topk(20, sum by (namespace) (acm_rs:namespace:memory_usage{cluster="$cluster", profile="$profile"}) / sum by (namespace) (acm_rs:namespace:memory_request{cluster="$cluster", profile="$profile"}))`,
+				dashboards.AddQueryDataSource(datasourceName),
+				query.SeriesNameFormat("{{namespace}}"),
+			),
+		),
+	)
+}
+
+func CPUQuotaTablePanel(datasourceName string) panelgroup.Option {
+	return panelgroup.AddPanel("CPU Quota Table",
+		panel.Description("CPU utilization, usage, request, recommendation, and request hard per namespace"),
+		TableWithLinks(TablePluginSpec{
+			ColumnSettings: []ColumnSettingsWithLink{
+				{ColumnSettings: tablePanel.ColumnSettings{Name: "timestamp", Hide: true}},
+				nsTblCol("namespace", "Namespace", tablePanel.LeftAlign, nil),
+				nsTblCol("value #1", "CPU Utilization %", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.PercentDecimalUnit, DecimalPlaces: 2},
+					func(c *ColumnSettingsWithLink) { c.Sort = tablePanel.DescSort }),
+				nsTblCol("value #2", "CPU Usage", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.DecimalUnit, DecimalPlaces: 2}),
+				nsTblCol("value #3", "CPU Request", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.DecimalUnit, DecimalPlaces: 2}),
+				nsTblCol("value #4", "CPU Recommendation", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.DecimalUnit, DecimalPlaces: 2}),
+				nsTblCol("value #5", "CPU Request Hard", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.DecimalUnit, DecimalPlaces: 2}),
+			},
+			CellSettings: []tablePanel.CellSettings{
+				{Condition: tablePanel.Condition{Kind: tablePanel.MiscConditionKind, Spec: &tablePanel.MiscConditionSpec{Value: tablePanel.NullValue}}, Text: "N/A"},
+			},
+			Transforms: []commonSdk.Transform{
+				{Kind: commonSdk.MergeSeriesKind, Spec: commonSdk.MergeSeriesSpec{}},
+			},
+			EnableFiltering: true,
+		}),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:cpu_usage{cluster="$cluster", profile="$profile"})[$days:]) / max_over_time(sum by (namespace) (acm_rs:namespace:cpu_request{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:cpu_usage{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:cpu_request{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:cpu_recommendation{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:cpu_request_hard{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+	)
+}
+
+func MemQuotaTablePanel(datasourceName string) panelgroup.Option {
+	return panelgroup.AddPanel("Memory Quota Table",
+		panel.Description("Memory utilization, usage, request, recommendation, and request hard per namespace"),
+		TableWithLinks(TablePluginSpec{
+			ColumnSettings: []ColumnSettingsWithLink{
+				{ColumnSettings: tablePanel.ColumnSettings{Name: "timestamp", Hide: true}},
+				nsTblCol("namespace", "Namespace", tablePanel.LeftAlign, nil),
+				nsTblCol("value #1", "Memory Utilization %", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.PercentDecimalUnit, DecimalPlaces: 2},
+					func(c *ColumnSettingsWithLink) { c.Sort = tablePanel.DescSort }),
+				nsTblCol("value #2", "Memory Usage", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.BytesUnit, DecimalPlaces: 2}),
+				nsTblCol("value #3", "Memory Request", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.BytesUnit, DecimalPlaces: 2}),
+				nsTblCol("value #4", "Memory Recommendation", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.BytesUnit, DecimalPlaces: 2}),
+				nsTblCol("value #5", "Memory Request Hard", tablePanel.RightAlign,
+					&commonSdk.Format{Unit: &dashboards.BytesUnit, DecimalPlaces: 2}),
+			},
+			CellSettings: []tablePanel.CellSettings{
+				{Condition: tablePanel.Condition{Kind: tablePanel.MiscConditionKind, Spec: &tablePanel.MiscConditionSpec{Value: tablePanel.NullValue}}, Text: "N/A"},
+			},
+			Transforms: []commonSdk.Transform{
+				{Kind: commonSdk.MergeSeriesKind, Spec: commonSdk.MergeSeriesSpec{}},
+			},
+			EnableFiltering: true,
+		}),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:memory_usage{cluster="$cluster", profile="$profile"})[$days:]) / max_over_time(sum by (namespace) (acm_rs:namespace:memory_request{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:memory_usage{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:memory_request{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:memory_recommendation{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+		panel.AddQuery(
+			query.PromQL(
+				`max_over_time(sum by (namespace) (acm_rs:namespace:memory_request_hard{cluster="$cluster", profile="$profile"})[$days:])`,
+				dashboards.AddQueryDataSource(datasourceName),
+			),
+		),
+	)
+}
