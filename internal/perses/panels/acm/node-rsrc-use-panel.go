240e40e7aa9015032bd2f312cf056ba3e3e78c0d
Darshan Vandra <dvandra@redhat.com>
2026-05-07 17:17:04 +0530

    add configmap for in-memory-predicate-evaluation
    Signed-off-by: Darshan Vandra <dvandra@redhat.com>

--- internal/perses/panels/acm/node-rsrc-use-panel.go
+++ internal/perses/panels/acm/node-rsrc-use-panel.go
@@ -36,7 +36,7 @@ func NodeCPUUtilization(datasourceName string, labelMatchers ...promql.LabelMatc
 		panel.AddQuery(
 			query.PromQL(
 				promql.SetLabelMatchers(
-					"instance:node_cpu_utilisation:rate1m{cluster=\"$cluster\", job=\"node-exporter\", instance=~\"$instance\"}",
+					"instance:node_cpu_utilisation:rate1m{cluster=\"$cluster\", job=\"node-exporter\", instance=~\"$instance\"}", //nolint:misspell // upstream metric name
 					labelMatchers,
 				),
 				query.SeriesNameFormat("{{ instance }}"),
@@ -109,7 +109,7 @@ func NodeMemoryUtilization(datasourceName string, labelMatchers ...promql.LabelM
 		panel.AddQuery(
 			query.PromQL(
 				promql.SetLabelMatchers(
-					"instance:node_memory_utilisation:ratio{cluster=\"$cluster\", job=\"node-exporter\", instance=~\"$instance\"}",
+					"instance:node_memory_utilisation:ratio{cluster=\"$cluster\", job=\"node-exporter\", instance=~\"$instance\"}", //nolint:misspell // upstream metric name
 					labelMatchers,
 				),
 				query.SeriesNameFormat("{{ instance }}"),
