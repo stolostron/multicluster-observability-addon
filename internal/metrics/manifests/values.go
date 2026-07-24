374bd44cbc6ad9f6c0406af4bb4241f493747db0
Thibault Mange <22740367+thibaultmg@users.noreply.github.com>
2026-07-24 08:13:18 +0200

    improve api version check in helm chart
    Signed-off-by: Thibault Mange <22740367+thibaultmg@users.noreply.github.com>

--- internal/metrics/manifests/values.go
+++ internal/metrics/manifests/values.go
@@ -240,9 +240,10 @@ func BuildValues(opts handlers.Options) (*MetricsValues, error) {
 		}
 
 		ret.Platform.Rules = append(ret.Platform.Rules, ConfigValue{
-			Name:   rule.Name,
-			Data:   string(ruleJson),
-			Labels: rule.Labels,
+			Name:       rule.Name,
+			Data:       string(ruleJson),
+			Labels:     rule.Labels,
+			APIVersion: rule.APIVersion,
 		})
 	}
 
@@ -253,9 +254,10 @@ func BuildValues(opts handlers.Options) (*MetricsValues, error) {
 		}
 
 		configValueItem := ConfigValue{
-			Name:   rule.Name,
-			Data:   string(ruleJson),
-			Labels: rule.Labels,
+			Name:       rule.Name,
+			Data:       string(ruleJson),
+			Labels:     rule.Labels,
+			APIVersion: rule.APIVersion,
 		}
 		targetNamespace := rule.Annotations[config.TargetNamespaceAnnotation]
 		if targetNamespace != "" {
