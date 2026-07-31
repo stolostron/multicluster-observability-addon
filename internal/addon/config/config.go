6e7637eb5fe320721c047da9a3b2cdec654e1a06
Guido Ricioppo <griciopp@redhat.com>
2026-07-31 09:30:43 +0200

    [ACM-30183] feat: TLS Profiles consistency (#534)
    Add TLS utilities to retrieve the TLS Profile (cypers and min version) from the OCP Cluster API Server. Intermidiate profile is used by default for not-OCP clusters.
    
    Operator: to watch API Server tls profile changes and set secure metrics endpoint.
    
    Operands: use readOnly feedbackRules to read ocm-tls-profile configMap on managed clusters and configure kubeRbacProxy tls profile flags accordingly.
    Propagate tls profile to prometheus-operator, thanos-operator, endpoint-monitoring-operator.
    
    Add feedbackRules and tls utils test.
    
    Signed-off-by: Guido Ricioppo <griciopp@redhat.com>

--- internal/addon/config/config.go
+++ internal/addon/config/config.go
@@ -72,6 +72,17 @@ const (
 	IsOLMManagedFeedbackName              = "isOLMManaged"
 	IsOLMManagedFeedbackPath              = `.metadata.labels.olm\.managed`
 
+	// TLS profile feedback
+	TLSProfileConfigMapNamespace = "open-cluster-management-agent"
+	TLSProfileConfigMapName      = "ocm-tls-profile"
+	TLSProfileTypeFeedbackName   = "profileType"
+	TLSProfileTypeFeedbackPath   = ".data.profileType"
+	TLSMinVersionFeedbackName    = "minTLSVersion"
+	TLSMinVersionFeedbackPath    = ".data.minTLSVersion"
+	TLSCipherSuitesFeedbackName  = "cipherSuites"
+	TLSCipherSuitesFeedbackPath  = ".data.cipherSuites"
+	TLSDefaultMinVersion         = "VersionTLS12"
+
 	VendorOverrideAnnotationKey = "mcoa-override-vendor"
 	AnnotationOriginalResource  = "mcoa.openshift.io/original-resource"
 )
--- internal/metrics/resource/agentssa.go
+++ internal/metrics/resource/agentssa.go
@@ -90,7 +90,7 @@ type PrometheusAgentSSA struct {
 	desiredAgent *cooprometheusv1alpha1.PrometheusAgent
 }
 
-// Build generate the prometheusAgent resource containing only fields that must be enforced using server-side apply.
+// Build generates the prometheusAgent resource containing only fields that must be enforced using server-side apply.
 func (p *PrometheusAgentSSA) Build() *cooprometheusv1alpha1.PrometheusAgent {
 	p.desiredAgent = &cooprometheusv1alpha1.PrometheusAgent{
 		TypeMeta: p.ExistingAgent.TypeMeta,
@@ -260,10 +260,9 @@ func (p *PrometheusAgentSSA) setKubeRBACProxySidecar() {
 	}
 
 	p.desiredAgent.Spec.Volumes = append(p.desiredAgent.Spec.Volumes, newVolumes...)
-	p.desiredAgent.Spec.Containers = append(
-		p.desiredAgent.Spec.Containers,
-		p.createKubeRbacProxyContainer(),
-	)
+
+	container := p.createKubeRbacProxyContainer()
+	p.desiredAgent.Spec.Containers = append(p.desiredAgent.Spec.Containers, container)
 }
 
 func (p PrometheusAgentSSA) createKubeRbacProxyContainer() corev1.Container {
