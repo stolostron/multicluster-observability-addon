f6ec407126ccc56f6162c18a72751a04eb344c66
Guido Ricioppo <griciopp@redhat.com>
2026-07-31 12:40:48 +0200

    fix duplicate code
    After rebase
    
    Signed-off-by: Guido Ricioppo <griciopp@redhat.com>

--- internal/metrics/handlers/handler.go
+++ internal/metrics/handlers/handler.go
@@ -164,26 +164,6 @@ func (o *OptionsBuilder) Build(ctx context.Context, mcAddon *addonapiv1beta1.Man
 		ret.TLSCipherSuites = *cipherValues[0].Value.String
 	}
 
-	// Read TLS profile from ManifestWork feedback
-	tlsProfileCM := workv1.ResourceIdentifier{
-		Group:     "",
-		Resource:  "configmaps",
-		Name:      addoncfg.TLSProfileConfigMapName,
-		Namespace: addoncfg.TLSProfileConfigMapNamespace,
-	}
-	feedback, err := common.GetFeedbackValuesForResources(ctx, o.Client, managedCluster.Name, addoncfg.Name, tlsProfileCM)
-	if err != nil {
-		return ret, fmt.Errorf("failed to get TLS profile feedback: %w", err)
-	}
-
-	tlsFeedback := feedback[tlsProfileCM]
-	if minVersionValues := common.FilterFeedbackValuesByName(tlsFeedback, addoncfg.TLSMinVersionFeedbackName); len(minVersionValues) > 0 && minVersionValues[0].Value.String != nil {
-		ret.TLSMinVersion = *minVersionValues[0].Value.String
-	}
-	if cipherValues := common.FilterFeedbackValuesByName(tlsFeedback, addoncfg.TLSCipherSuitesFeedbackName); len(cipherValues) > 0 && cipherValues[0].Value.String != nil {
-		ret.TLSCipherSuites = *cipherValues[0].Value.String
-	}
-
 	caTargetName := config.GetHubMtlsCASecretName(config.GetTrimmedClusterID(ret.HubClusterID))
 	certTargetName := config.GetHubMtlsCertSecretName(config.GetTrimmedClusterID(ret.HubClusterID))
 
