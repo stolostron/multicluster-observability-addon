b47a45029d59ac1f08d860854228592e5d680b1d
George Aeillo <gaeillo@redhat.com>
2026-08-27 09:34:51 -0400

    Switch checks to pass linter and remove the type switch
    Signed-off-by: George Aeillo <gaeillo@redhat.com>

--- internal/logging/handlers/create_default_stack_resources_test.go
+++ internal/logging/handlers/create_default_stack_resources_test.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"context"
+	"fmt"
 	"testing"
 
 	lokiv1 "github.com/grafana/loki/operator/api/loki/v1"
@@ -9,7 +9,6 @@ import (
 	"github.com/stolostron/multicluster-observability-addon/internal/addon"
 	"github.com/stolostron/multicluster-observability-addon/internal/addon/common"
 	addoncfg "github.com/stolostron/multicluster-observability-addon/internal/addon/config"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -17,12 +16,12 @@ import (
 	"k8s.io/klog/v2"
 	addonapiv1beta1 "open-cluster-management.io/api/addon/v1beta1"
 	clusterv1 "open-cluster-management.io/api/cluster/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 func TestDefaultStackResourcesSurviveDeleteOrphan(t *testing.T) {
+	ctx := t.Context()
 	scheme := runtime.NewScheme()
 	require.NoError(t, addonapiv1beta1.Install(scheme))
 	require.NoError(t, clusterv1.Install(scheme))
@@ -47,35 +46,21 @@ func TestDefaultStackResourcesSurviveDeleteOrphan(t *testing.T) {
 
 	fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(cmao).Build()
 
-	objs, _, err := BuildDefaultStackResources(t.Context(), fakeClient, cmao, addon.LogsOptions{DefaultStack: true}, addon.LogsOptions{}, "hub.example.com")
+	objs, _, err := BuildDefaultStackResources(ctx, fakeClient, cmao, addon.LogsOptions{DefaultStack: true}, addon.LogsOptions{}, "hub.example.com")
 	require.NoError(t, err)
 
-	var clf *loggingv1.ClusterLogForwarder
-	var ls *lokiv1.LokiStack
 	for _, obj := range objs {
-		switch o := obj.(type) {
-		case *loggingv1.ClusterLogForwarder:
-			clf = o
-		case *lokiv1.LokiStack:
-			ls = o
-		}
-	}
-	require.NotNil(t, clf, "expected a default-stack ClusterLogForwarder")
-	require.NotNil(t, ls, "expected a default-stack LokiStack")
-
-	for _, obj := range []client.Object{clf, ls} {
 		require.NoError(t, controllerutil.SetControllerReference(cmao, obj, scheme))
-		require.NoError(t, fakeClient.Create(context.Background(), obj))
+		require.NoError(t, fakeClient.Create(ctx, obj))
 	}
 
-	require.NoError(t, common.DeleteOrphanResources(context.Background(), klog.Background(), fakeClient, cmao, &loggingv1.ClusterLogForwarderList{}))
-	require.NoError(t, common.DeleteOrphanResources(context.Background(), klog.Background(), fakeClient, cmao, &lokiv1.LokiStackList{}))
+	require.NoError(t, common.DeleteOrphanResources(ctx, klog.Background(), fakeClient, cmao, &loggingv1.ClusterLogForwarderList{}))
+	require.NoError(t, common.DeleteOrphanResources(ctx, klog.Background(), fakeClient, cmao, &lokiv1.LokiStackList{}))
 
-	gotCLF := &loggingv1.ClusterLogForwarder{}
-	err = fakeClient.Get(context.Background(), types.NamespacedName{Name: clf.Name, Namespace: clf.Namespace}, gotCLF)
-	assert.NoError(t, err, "CMAO-owned ClusterLogForwarder with a matching placement annotation must not be deleted")
-
-	gotLS := &lokiv1.LokiStack{}
-	err = fakeClient.Get(context.Background(), types.NamespacedName{Name: ls.Name, Namespace: ls.Namespace}, gotLS)
-	assert.NoError(t, err, "CMAO-owned LokiStack with a matching placement annotation must not be deleted")
+	key := types.NamespacedName{
+		Name:      fmt.Sprintf("%s-%s", addoncfg.DefaultStackPrefix, addoncfg.GlobalPlacementName),
+		Namespace: addoncfg.InstallNamespace,
+	}
+	require.NoError(t, fakeClient.Get(ctx, key, &loggingv1.ClusterLogForwarder{}), "CMAO-owned ClusterLogForwarder with a matching placement annotation must not be deleted")
+	require.NoError(t, fakeClient.Get(ctx, key, &lokiv1.LokiStack{}), "CMAO-owned LokiStack with a matching placement annotation must not be deleted")
 }
