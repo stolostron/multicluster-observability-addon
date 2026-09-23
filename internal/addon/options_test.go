2735c7efde85543306402bb36ff4e7bafd4d58aa
Daniel Buchanan <dbuchana@redhat.com>
2026-09-23 09:24:17 -0400

    Remove default tenant from remote write url thanos (#667)
    Signed-off-by: Daniel Buchanan <dbuchana@redhat.com>

--- internal/addon/options_test.go
+++ internal/addon/options_test.go
@@ -80,7 +80,7 @@ func TestBuildOptions(t *testing.T) {
 						HubEndpoint: url.URL{
 							Scheme: "https",
 							Host:   "metrics.example.com",
-							Path:   "api/metrics/v1/default/api/v1/receive",
+							Path:   "api/metrics/v1/api/v1/receive",
 						},
 					},
 					AnalyticsOptions: AnalyticsOptions{
@@ -117,7 +117,7 @@ func TestBuildOptions(t *testing.T) {
 						HubEndpoint: url.URL{
 							Scheme: "https",
 							Host:   "metrics.example.com",
-							Path:   "api/metrics/v1/default/api/v1/receive",
+							Path:   "api/metrics/v1/api/v1/receive",
 						},
 					},
 					AnalyticsOptions: AnalyticsOptions{
